3014eee307c0ee9617db80b19ad260da2fd1e410
katekani nyamandi <katekaninyamandi@MacBookPro.omega.wwads.net>
2025-03-03 16:08:31 +0200

    push updates

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const { getPullRequests } = require('./src/consume_github_api'); 
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { getPullRequests } = require("./src/consume_github_api");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors()); 
+app.use(cors());
 
-app.get('/pulls', async (req, res) => {
- try {
-     const { owner, repo, startDate, endDate } = req.query;
-     const pullRequests = await getPullRequests({ owner, repo, startDate, endDate });
-     res.json(pullRequests);
- } catch (error) {
-     console.error(error);
-     res.status(500).json({ error: error.message });
- }
+app.get("/pulls", async (req, res) => {
+  try {
+    const { owner, repo, startDate, endDate } = req.query;
+    const pullRequests = await getPullRequests({
+      owner,
+      repo,
+      startDate,
+      endDate,
+    });
+    res.json(pullRequests);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
- console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
